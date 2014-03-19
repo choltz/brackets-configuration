0fbada2ea4be79b28a2616f2831866224ad98daf
Chris Holtz <choltz@gmail.com>
2014-03-19 10:24:55 -0400

    Added main.js file to house configuration tweaks

---
+++ main.js
@@ -0,0 +1,7 @@
+define(function (require, exports, module) {
+    "use strict";
+    
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    // ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: 'Ubuntu Mono'; font-size: 11pt;}");
+    ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: 'Ubuntu Mono'; font-size: 16pt; line-height: 22px;}");
+});
