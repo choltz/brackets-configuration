bfa8cfd38c99860833bb237f9b28f92e7052b4e3
Chris Holtz <choltz@gmail.com>
2014-03-19 10:35:05 -0400

    Cleaned up font configuration a bit.

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 define(function (require, exports, module) {
     "use strict";
-    
-    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
-    // ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: 'Ubuntu Mono'; font-size: 11pt;}");
-    ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: 'Ubuntu Mono'; font-size: 16pt; line-height: 22px;}");
+
+    // Set the editor font style
+    (function() {
+        var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+        var style = ".CodeMirror { \
+                      font-family: Ubuntu Mono; \
+                      font-size:   16pt; \
+                      line-height: 22px;}";
+
+        ExtensionUtils.addEmbeddedStyleSheet(style);
+    })();
+
 });
