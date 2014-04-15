1e61987805383fabe270b27f5b671aa8add294cf
Chris Holtz <choltz@gmail.com>
2014-04-15 14:27:38 -0400

    The latest brackets appears to remember font sizes, do not need that here anymore

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ define(function (require, exports, module) {
     "use strict";
 
     // Set the editor font style
+    
     (function() {
         var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
         var style = ".CodeMirror { \
-                      font-family: Ubuntu Mono; \
-                      font-size:   16pt; \
-                      line-height: 22px;}";
+                      font-family: Ubuntu Mono;}";
 
-        ExtensionUtils.addEmbeddedStyleSheet(style);
+              ExtensionUtils.addEmbeddedStyleSheet(style);
     })();
-
+    
 });
